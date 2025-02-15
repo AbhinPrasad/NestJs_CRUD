7cec038279efb819fc081ec3647239f5a32c671a
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-02-15 15:43:04 +0530

    completed middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,4 @@
-import {
-  Module,
-  NestModule,
-  MiddlewareConsumer,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
@@ -12,7 +7,6 @@ import { UsersModule } from './users/users.module';
 import { CommonController } from './common/common.controller';
 import { CommonModule } from './common/common.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
-import { FunctionalLogger } from './middlewares/functional.middleware';
 
 @Module({
   imports: [UsersModule, CommonModule],
@@ -21,12 +15,11 @@ import { FunctionalLogger } from './middlewares/functional.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware, FunctionalLogger)
-      .exclude({ path: 'users', method: RequestMethod.PATCH })
-      // .forRoutes('users'); --> for users route handler
-      // .forRoutes({ path: 'users', method: RequestMethod.GET }); --> for particular request method
-      .forRoutes(UsersController); //--> for a single controller
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    // .exclude({ path: 'users', method: RequestMethod.PATCH })
+    // .forRoutes('users'); --> for users route handler
+    // .forRoutes({ path: 'users', method: RequestMethod.GET }); --> for particular request method
+    // .forRoutes(UsersController); --> for a single controller
   }
 }
 
@@ -37,5 +30,10 @@ The configure() method can be made asynchronous using async/await
 
 The apply() method may either take a single middleware, or multiple arguments 
 to specify multiple middlewares.
+
+Accessing the DI container in a global middleware is not possible.
+You can use a functional middleware instead when using app.use(). 
+Alternatively, you can use a class middleware and consume it with .forRoutes('*') 
+within the AppModule (or any other module).
 
 */

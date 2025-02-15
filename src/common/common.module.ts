842e8de6eb95c41cc4a034257ec5592eb9126c8b
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-02-15 14:46:05 +0530

    Converted the common module to global

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
 
+@Global()
 @Module({
   controllers: [CommonController],
   providers: [CommonService],
 })
 export class CommonModule {}
+
+/*
+
+When you want to provide a set of providers which should be available everywhere out-of-the-box 
+(e.g., helpers, database connections, etc.), make the module global with the @Global() decorator.
+
+*/

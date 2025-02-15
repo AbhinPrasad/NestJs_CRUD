5a1bd321190c28580d8d8508398a0b4afe00e6c1
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-02-15 15:13:50 +0530

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
@@ -6,10 +6,15 @@ import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { CommonController } from './common/common.controller';
 import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [UsersModule, CommonModule],
   controllers: [AppController, UsersController, CommonController],
   providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('users'); // --> for users route handler
+  }
+}

901ded1fd19e544c909382db4b3b77b00bbe79e8
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-02-15 14:54:27 +0530

    global modules description comments

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -14,4 +14,16 @@ export class CommonModule {}
 When you want to provide a set of providers which should be available everywhere out-of-the-box 
 (e.g., helpers, database connections, etc.), make the module global with the @Global() decorator.
 
+The @Global() decorator makes the module global-scoped. Global modules should be registered
+only once, generally by the root or core module.
+Modules that wish to inject the service will not need to import the CommonModule in 
+their imports array.
+
+Making everything global is not recommended as a design practice. While global modules 
+can help reduce boilerplate, it's generally better to use the imports array to make 
+a module's API available to other modules in a controlled and clear way. 
+This approach provides better structure and maintainability, ensuring that only 
+the necessary parts of the module are shared with others while avoiding unnecessary 
+coupling between unrelated parts of the application.
+
 */
